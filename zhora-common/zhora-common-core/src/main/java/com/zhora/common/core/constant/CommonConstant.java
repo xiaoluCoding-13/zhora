fb3c5c7ac76c76d5f2a3c79967c54d5087605c2f
lzh <luzhehen@163.com>
2025-07-27 14:56:32 +0800

    添加依赖

---
+++ zhora-common/zhora-common-core/src/main/java/com/zhora/common/core/constant/CommonConstant.java
@@ -0,0 +1,19 @@
+package com.zhora.common.core.constant;
+
+/**
+ * 公共常量
+ *
+ * @author zhehen.lu
+ * @date 2024/6/24 10:53
+ */
+public interface CommonConstant {
+    /**
+     * traceId字段 跟log-spring-boot-starter 里面MDC设置的变量一致
+     */
+    public static final String TRACE_ID = "traceId";
+
+    /**
+     * 授权头
+     */
+    String AUTHORIZATION = "Authorization";
+}
